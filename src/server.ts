649202e14ed95727b567e9a21da952654498edb8
Vishnu P R <vishnuvicki3$@gmail.com>
2024-09-10 12:59:42 +0530

    rehosting

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ connectDB()
 
 const server = createServer(app)
 export const io = initializeSocketIO(server)
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000  
 server.listen(PORT, () => {
   console.log(`server is running on http://localhost:${PORT}`); 
 })
